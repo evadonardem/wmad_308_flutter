2300648764e57e8288ce8c801226029444565c8e
JessaCunana <202100015438@kcp.edu.ph>
2024-05-26 08:12:51 +0800

    activity_2_simple_state_management_jessa_cunanan

---
+++ activity_2_simple_state_management/cunanan_jessa/android/app/src/main/kotlin/com/example/jessa_cunanan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jessa_cunanan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ activity_2_simple_state_management/cunanan_jessa/lib/main.dart
@@ -0,0 +1,295 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//Fetch data from the internet (begin)
+const endpoint = 'https://jsonplaceholder.typicode.com/users';
+
+Future fetchUsers() async {
+  final response = await http.get(Uri.parse(endpoint));
+  if (response.statusCode == 200) {
+    final rawData = response.body;
+    final List jsonDecodeData = jsonDecode(rawData);
+    final users = jsonDecodeData.map((e) => User.fromJson(e));
+
+    print('Raw Data: $rawData');
+    print('JSON Decoded: $jsonDecodeData');
+    print('Users: $users');
+
+    return users;
+  } else {
+    throw Exception('Failed to load users');
+  }
+}
+
+//Create dart object to represent an Album
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final String phone;
+  final String website;
+
+  const User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.phone,
+    required this.website,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': int id,
+        'name': String name,
+        'username': String username,
+        'email': String email,
+        'phone': String phone,
+        'website': String website,
+      } =>
+        User(
+          id: id,
+          name: name,
+          username: username,
+          email: email,
+          phone: phone,
+          website: website,
+        ),
+      _ => throw const FormatException('Failed to load users.'),
+    };
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  bool isLoggedIn = false;
+  User? loggedInUser;
+
+  void papasukin(User $user) {
+    isLoggedIn = true;
+    loggedInUser = $user;
+    notifyListeners();
+  }
+
+  void palabasinNaSya(){
+    isLoggedIn = false;
+    loggedInUser = null;
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => MyAppState(),
+        child: MaterialApp(
+          title: 'Simple State Management',
+          theme: ThemeData(
+            colorScheme:
+                ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 15, 163, 76)),
+            useMaterial3: true,
+            textTheme: const TextTheme(
+              bodyMedium: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          home: const MyHomePage(title: 'Welcome'),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.isLoggedIn) {
+      final String userDetail = "Welcome ${appState.loggedInUser!.name} !";
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(userDetail),
+        ),
+        body: const Center(child: Text('Jessa Cunanan')),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.wine_bar),
+              label: 'Bar',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+          currentIndex: 0,
+          selectedItemColor: Color.fromARGB(255, 248, 23, 162),
+          unselectedItemColor: Color.fromARGB(255, 247, 19, 247),
+          onTap: (index) {
+            print(index);
+            if (index == 3) {
+              appState.palabasinNaSya();
+            }
+          },
+        ),
+      );
+    }
+    return const Scaffold(
+      body: Center(
+        child: JessaLoginPage(),
+      ),
+    );
+  }
+}
+
+class JessaLoginPage extends StatefulWidget {
+  const JessaLoginPage({
+    super.key,
+  });
+
+  @override
+  State<JessaLoginPage> createState() => _JessaLoginPageState();
+}
+
+class _JessaLoginPageState extends State<JessaLoginPage> {
+  late String jessaUsernameEntered;
+  late String jessaPasswordEntered;
+  late Future jessaFutureUsers;
+
+  @override
+  void initState() {
+    super.initState();
+    jessaPasswordEntered = "";
+    jessaUsernameEntered = "";
+    jessaFutureUsers = fetchUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(
+        fontSize: 15,
+        color: Color.fromRGBO(233, 7, 7, 1),
+        fontWeight: FontWeight.bold,
+      ),
+      minimumSize: const Size(300.0, 50.0),
+      backgroundColor: (Color.fromARGB(183, 127, 101, 245)),
+    );
+
+    print('Entered username: $jessaUsernameEntered');
+    print('Entered password: $jessaPasswordEntered');
+
+    return Padding(
+      padding: const EdgeInsets.all(90),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(50),
+                child: Column(
+                  children: [
+                    Text(' Login '),
+                    const SizedBox(height: 15),
+                    const Divider(
+                      thickness: 2.0,
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Username',
+                      ),
+                      onChanged: (String jessaUsername) {
+                        print("This is the username: $jessaUsername");
+                        setState(() {
+                          jessaUsernameEntered = jessaUsername;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                      onChanged: (String jessaPassword) {
+                        print("This is the password: $jessaPassword");
+                        setState(() {
+                          jessaPasswordEntered = jessaPassword;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 50),
+                    ElevatedButton(
+                      style: style,
+                      onPressed: () {
+                        jessaFutureUsers.whenComplete(() {
+                          print('Users successfully loaded...');
+                        }).then((users) {
+                          final bool isValidUser = users
+                              .where((user) =>
+                                  (user.username == jessaUsernameEntered ||
+                                      user.email == jessaUsernameEntered))
+                              .isNotEmpty;
+                          print('User found?: $isValidUser');
+                          if (isValidUser) {
+                            final User authenticatedUser = users
+                                .where((user) =>
+                                    (user.username == jessaUsernameEntered ||
+                                        user.email == jessaUsernameEntered))
+                                .first;
+                            print('This is the username $authenticatedUser');
+                            appState.papasukin(authenticatedUser);
+                          }
+                        });
+                        print("Checking Credentials");
+                      },
+                      child: FutureBuilder(
+                          future: jessaFutureUsers,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return const Text('Login');
+                            }
+                            return const CircularProgressIndicator();
+                          }),
+                    ),
+                  ],
+                ),
+              )),
+          SizedBox(height: 10),
+          Text('Login Page by Jessa'),
+        ],
+      ),
+    );
+  }
+}
